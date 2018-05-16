68bd5d2999dc7f09f2606c353cf2133e249c760a
Albert Chen <blahoink@gmail.com>
2018-05-16 15:44:06 -0400

    update shell scripts

--- copy_to_gdrive.sh
+++ copy_to_gdrive.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-rsync -av --progress python /gd/MS/SCoPE/bayesian_RT/python --exclude-from="./rsync_exclude.txt"
+rsync -av --progress python /gd/MS/SCoPE/bayesian_RT --exclude-from="./rsync_exclude.txt"
---
+++ python/install_from_git.sh
@@ -0,0 +1,3 @@
+git clone https://blahoink:mala3dofu@github.com/blahoink/RTLib ~/RTLib
+cd ~/RTLib/rtlib
+pip install ./
