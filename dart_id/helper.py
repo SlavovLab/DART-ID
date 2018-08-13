7fba0b6a9ec9c8797fa2d30d92c61ba85345793b
Albert Chen <blahoink@gmail.com>
2018-08-13 17:32:06 -0400

    add weighted quantiles function for possible future use

--- dart_id/helper.py
+++ dart_id/helper.py
@@ -146,5 +146,50 @@ def read_config_file(args, create_output_folder=True):
 # TODO: do this dumb input check every time the config file is loaded
 def check_config_inputs(config):
   pass
+
+# pulled from: https://stackoverflow.com/a/29677616
+def weighted_quantile(values, quantiles, sample_weight=None, values_sorted=False, old_style=False):
+  """ Very close to numpy.percentile, but supports weights.
+  NOTE: quantiles should be in [0, 1]!
+  :param values: numpy.array with data
+  :param quantiles: array-like with many quantiles needed
+  :param sample_weight: array-like of the same length as `array`
+  :param values_sorted: bool, if True, then will avoid sorting of initial array
+  :param old_style: if True, will correct output to be consistent with numpy.percentile.
+  :return: numpy.array with computed quantiles.
+  """
+  
+  values = np.array(values)
+  quantiles = np.array(quantiles)
+
+  if sample_weight is None:
+    sample_weight = np.ones(len(values))
+  sample_weight = np.array(sample_weight)
+  assert np.all(quantiles >= 0) and np.all(quantiles <= 1), 'quantiles should be in [0, 1]'
+
+  if not values_sorted:
+    sorter = np.argsort(values)
+    values = values[sorter]
+    sample_weight = sample_weight[sorter]
+
+  weighted_quantiles = np.cumsum(sample_weight) - 0.5 * sample_weight
+  if old_style:
+    # To be convenient with np.percentile
+    weighted_quantiles -= weighted_quantiles[0]
+    weighted_quantiles /= weighted_quantiles[-1]
+  else:
+    weighted_quantiles /= np.sum(sample_weight)
+  return np.interp(quantiles, weighted_quantiles, values)
+
+def weighted_median(values, weights):
+  order = np.argsort(values)
+  values = values[order]
+  weights = weights[order]
+
+  #weighted_quantiles = (np.cumsum(weights) - (0.5 * weights)) / np.sum(weights)
+  
+  return np.interp([0.5], 
+    (np.cumsum(weights) - (0.5 * weights)) / np.sum(weights),
+    values)
     
 
